2bccb225674a0b0e1dc8879e3ebd6a11005e1eea
pvh1602 <pvh16021998@gmail.com>
2021-04-18 05:41:53 +0700

    update

--- script/Resnet_backbone/Resnet_backbone_size256.sh
+++ script/Resnet_backbone/Resnet_backbone_size256.sh
@@ -2,16 +2,16 @@ cd ../../
 pwd
 
 
-python train.py --backbone Resnet18 --n_filters 5 --bridge MHSABridge --n_block 2 --train_size 352
-python train.py --backbone Resnet18 --n_filters 5 --bridge MHSABridge --n_block 3 --train_size 352
+# python train.py --backbone Resnet18 --n_filters 5 --bridge MHSABridge --n_block 2 --train_size 352
+# python train.py --backbone Resnet18 --n_filters 5 --bridge MHSABridge --n_block 3 --train_size 352
 
 
-python train.py --backbone Resnet34 --n_filters 5 --bridge MHSABridge --n_block 1 --train_size 352
-python train.py --backbone Resnet34 --n_filters 5 --bridge MHSABridge --n_block 2 --train_size 352
-python train.py --backbone Resnet34 --n_filters 5 --bridge MHSABridge --n_block 3 --train_size 352
+# python train.py --backbone Resnet34 --n_filters 5 --bridge MHSABridge --n_block 1 --train_size 352
+# python train.py --backbone Resnet34 --n_filters 5 --bridge MHSABridge --n_block 2 --train_size 352
+# python train.py --backbone Resnet34 --n_filters 5 --bridge MHSABridge --n_block 3 --train_size 352
 
 
-python train.py --backbone Resnet50 --n_filters 5 --bridge MHSABridge --n_block 1 --train_size 352
+python train.py --backbone Resnet50 --n_filters 5 --bridge MHSABridge --n_block 1 --train_size 352  # running at epoch 9 
 python train.py --backbone Resnet50 --n_filters 5 --bridge MHSABridge --n_block 2 --train_size 352
 python train.py --backbone Resnet50 --n_filters 5 --bridge MHSABridge --n_block 3 --train_size 352
 
